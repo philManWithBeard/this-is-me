6f63c1ace87ea45bcee7ac86c8c8407d5aa16bea
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-16 13:42:27 +0000

    Bring in button, field, form and input components

--- src/Pages/Postcode.jsx
+++ src/Pages/Postcode.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useAppState } from "../Components/state";
+import { Field } from "../Components/Forms/Field";
+import { Button } from "../Components/Forms/Button";
+import { Form } from "../Components/Forms/Form";
+import { Input } from "../Components/Forms/Input";
 
 const Postcode = () => {
   return (
