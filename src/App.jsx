af890e968fe988ff317d746cec05791914c98c83
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-16 12:51:35 +0000

    Router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import Dashboard from './Pages/Dashboard/Index'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import DefaultPage from './Components/DefaultPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./Components/state";
+import Postcode from "./Pages/Postcode";
+import Statistics from "./Pages/Statistics";
+import Who from "./Pages/Who";
+import Life from "./Pages/Life";
+import Scale from "./Pages/Scale";
+import Affect from "./Pages/Affect";
+import Confirmation from "./Pages/Confirmation";
+import ThankYou from "./Pages/ThankYou";
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      {/* <h1>House Prices vs Salary</h1> */}
-      {/* <Dashboard /> */}
-      <DefaultPage/>
-    </>
-  )
-}
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Postcode />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/who" element={<Who />} />
+          <Route path="/life" element={<Life />} />
+          <Route path="/scale" element={<Scale />} />
+          <Route path="/affect" element={<Affect />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
