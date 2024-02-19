66e67f5f368b3257d1604e87ffa89d6699da51ed
Setsu-Adachi <setsmob@gmail.com>
2024-02-19 07:08:07 +0000

    design header and footer

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import '../../index.css';
 
 const Footer = () => {
  return (
     <>
-    <h2>Footer</h2>
+        <footer className="custom-bg-light-sage d-flex align-items-center justify-content-center pt-5">
+            <h7 className="text-dark fst-italic">©2024 We Are Us</h7>
+        </footer>
     </>
  )   
 }
