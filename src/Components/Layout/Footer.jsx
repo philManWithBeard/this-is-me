6767fc68d0298ba32ac79adb9da6fb8e3c1eadb9
Hamdi <hamdishh2003@gmail.com>
2024-02-20 19:35:37 +0000

    fixed padding for footer text to get it centered

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ import "../../index.css";
 const Footer = () => {
   return (
     <>
-      <footer className="custom-bg-light-sage pt-5">
+      <footer className="custom-bg-light-sage pt-5" style={{ paddingBottom: '30px'}}>
         <h5 className="text-dark fst-italic  d-flex align-items-center justify-content-center">
           Made with ❤️ by We Are Four
         </h5>
