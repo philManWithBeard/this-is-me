bb03e86fdd676b227f480b2e5169db29f392b33d
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-20 21:16:26 +0000

    remove index.css

--- src/Components/Forms/Button.jsx
+++ src/Components/Forms/Button.jsx
@@ -1,22 +1,23 @@
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
-import '../../index.css';
 
-export const Button = forwardRef(({ children, variant = "primary", ...props }, ref) => {
-  const buttonRef = useRef();
+export const Button = forwardRef(
+  ({ children, variant = "primary", ...props }, ref) => {
+    const buttonRef = useRef();
 
-  useImperativeHandle(ref, () => ({
-    click: () => {
-      buttonRef.current?.click();
-    },
-  }));
+    useImperativeHandle(ref, () => ({
+      click: () => {
+        buttonRef.current?.click();
+      },
+    }));
 
-  return (
-    <button
-      className={`btn btn-lg btn-${variant} mt-3 custom-button`}
-      {...props}
-      ref={buttonRef}
-    >
-      {children}
-    </button>
-  );
-});
+    return (
+      <button
+        className={`btn btn-lg btn-${variant} mt-3 custom-button`}
+        {...props}
+        ref={buttonRef}
+      >
+        {children}
+      </button>
+    );
+  }
+);
