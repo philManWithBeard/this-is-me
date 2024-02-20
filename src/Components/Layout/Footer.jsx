f836100d0d95f80e92d6b53eac5133c824517eee
Setsu-Adachi <setsmob@gmail.com>
2024-02-20 19:30:53 +0000

    footer padding added to place text in the middle

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -5,11 +5,11 @@ import "../../index.css";
 const Footer = () => {
   return (
     <>
-      <footer className="custom-bg-light-sage pt-5">
-        <h5 className="text-dark fst-italic  d-flex align-items-center justify-content-center">
-          Made with ❤️ by We Are Four
-        </h5>
-      </footer>
+<footer className="custom-bg-light-sage pt-5" style={{ paddingBottom: '30px' }}>
+  <h5 className="text-dark fst-italic d-flex align-items-center justify-content-center">
+    Made with ❤️ by We Are Four
+  </h5>
+</footer>
     </>
   );
 };
