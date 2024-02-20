90121825378b55df53cd4d7e1957383f471645f2
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-20 21:00:13 +0000

    removed index.css and questions

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
-import "../../index.css";
 
 const Footer = () => {
   return (
     <>
-      <footer className="custom-bg-light-sage pt-5">
-        <h5 className="text-dark fst-italic  d-flex align-items-center justify-content-center">
+      <footer className="custom-bg-light-sage py-3 text-center">
+        <h5 className="text-dark fst-italic m-0">
           Made with ❤️ by We Are Four
         </h5>
       </footer>
